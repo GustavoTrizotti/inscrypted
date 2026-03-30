2ebf72e09fc8f63210dd87f0193ffa21c97699c6
Gustavo Trizotti <gustavo.trizotti@aluno.ifsp.edu.br>
2026-03-30 17:32:20 -0300

    initial project setup

---
+++ src/main/java/br/edu/ifsp/scl/inscrypted/InscryptedApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.inscrypted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscryptedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscryptedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/scl/inscrypted/InscryptedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.inscrypted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscryptedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
