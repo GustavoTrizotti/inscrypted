de91c33ccc8292069e09c0cfb5c676cd8f67526a
Gustavo Trizotti <gustavo.trizotti@aluno.ifsp.edu.br>
2026-03-30 17:48:03 -0300

    refac: change per-test game instances to BeforeEach approach

--- src/test/java/br/edu/ifsp/scl/inscrypted/GameTest.java
+++ src/test/java/br/edu/ifsp/scl/inscrypted/GameTest.java
@@ -1,24 +1,29 @@
 package br.edu.ifsp.scl.inscrypted;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+        game.drawInitialHand();
+    }
+
     @Test
     @DisplayName("Should give five cards to the player in initial hand")
     void shouldGiveFiveCardsToThePlayerInInitialHand() {
-        Game game = new Game();
-        game.drawInitialHand();
         assertThat(game.getHand().size()).isEqualTo(5);
     }
 
     @Test
     @DisplayName("Should have one squirrel card among five initial cards in hand")
     void shouldHaveOneSquirrelCardAmongFiveInitialCardsInHand() {
-        Game game = new Game();
-        game.drawInitialHand();
         int squirrels = (int) game.getHand().stream()
                 .map(Card::getTag)
                 .filter("Squirrel"::equals)
